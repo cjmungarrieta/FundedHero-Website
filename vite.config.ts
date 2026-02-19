b1a0b08997c85301e687bf3958658bc3d8dcb840
EliteCodeDev <elitecode2025dev@gmail.com>
2026-02-19 17:26:47 -0500

    dist

--- vite.config.ts
+++ vite.config.ts
@@ -2,8 +2,10 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  base: '/wp-content/themes/generatepress-child/',
   plugins: [react()],
   build: {
     outDir: 'dist',
+    assetsDir: 'assets',
   },
 });
